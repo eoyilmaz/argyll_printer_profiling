6a23e727acb5b9c2acd68c3a096b672b6f47397e
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-09 17:08:02 +0300

    added output directory creation

--- 2_generate_targets.bat
+++ 2_generate_targets.bat
@@ -4,6 +4,8 @@ rem targen -v -d2 -c <preconditioning.icc> -G -g32 -f840 <name>
 IF "%PAPERSIZE%"=="A4" set PATCHCOUNT=420
 IF "%PAPERSIZE%"=="A3" set PATCHCOUNT=460
 
+mkdir Outputs\%PRINTERBRAND%_%PRINTERMODEL%
+
 targen -v -d2 -G -g32 -f%PATCHCOUNT% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILENAME%
 printtarg -v -iCM -h -R1 -T300 -p %PAPERSIZE% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILENAME%
 
