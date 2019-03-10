acfe01e447b61a8220bd468c7db1f9a58960aaa2
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-11 01:45:56 +0300

    - Updated ``5_generate_profile.cmd`` to reflect the new name of the next command.

--- 5_generate_profile.cmd
+++ 5_generate_profile.cmd
@@ -2,4 +2,4 @@
 colprof -v -qh -r1.0 -S AdobeRGB.icc -cmt -dpp -D"%PROFILENAME%" Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILENAME%
 
 echo Step 5 Finished!
-echo Run Next Step (6_profile_check)
+echo Run Next Step (6_check_profile)
