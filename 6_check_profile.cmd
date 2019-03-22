8ed4434dafce111dbe0af64ab41a7226994b3e1b
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-22 16:39:29 +0300

    - Output path now contains the date

--- 6_check_profile.cmd
+++ 6_check_profile.cmd
@@ -1,5 +1,5 @@
 @echo off
-profcheck -k -v2 Outputs/%PRINTERBRAND%_%PRINTERMODEL%/%PROFILENAME%.ti3 Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILENAME%.icm
+profcheck -k -v2 Outputs/%PRINTERBRAND%_%PRINTERMODEL%/%PROFILEDATE%/%PROFILENAME%.ti3 Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%.icm
 
 echo Step 6 Finished!
 echo Run Next Step (7_install_profile)
