3c6a09a570cf7d9c9eecb4c71d10f8a4bec8e0a7
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-22 16:43:55 +0300

    - It is now possible to choose between hires mode or normal mode for pathces. Where the hires mode uses the i1pro patch pattern.

--- 2_generate_targets.cmd
+++ 2_generate_targets.cmd
@@ -1,17 +1,19 @@
 @echo off
 mkdir Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%
 
-IF "%PRECONDITION_PROFILE_PATH%" == "" (
+if "%PRECONDITION_PROFILE_PATH%" == "" (
     targen -v -d2 -G -g16 -f%PATCH_COUNT% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
-) ELSE (
+) else (
     targen -v -d2 -G -g16 -f%PATCH_COUNT% -c %PRECONDITION_PROFILE_PATH% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
 )
 
-rem use ColorMunki Photo as the device
-rem printtarg -v -iCM -h -R1 -T300 -M6 -L -P -p %PAPERSIZE% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
-
-rem use i1 pro as the device to print but later use ColorMunki Photo to read
-printtarg -v -ii1 -h -R1 -T300 -M2 -L -P -p %PAPERSIZE% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+if "%USE_HIGH_DENSITY_MODE%"=="True" (
+    rem use i1 pro as the device to print but later use ColorMunki Photo to read
+    printtarg -v -ii1 -h -R1 -T300 -M2 -L -P -p %PAPERSIZE% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+) else (
+    rem use ColorMunki Photo as the device
+    printtarg -v -iCM -h -R1 -T300 -M2 -L -P -p %PAPERSIZE% Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+)
 
 echo Step 2 Finished!
 echo Run Next Step (3_print_charts)
