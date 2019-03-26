494a72496fe13f6b5428431fb47e3d00c97295b1
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-27 02:56:07 +0300

    - 3_print_charts.cmd now uses Dry Creek Profile Printer as the default profile printing utility and falls back to Adobe Color Printer Utility if it can not find it.

--- 3_print_charts.cmd
+++ 3_print_charts.cmd
@@ -1,16 +1,34 @@
 @echo off
+
+set DRY_CREEK_PHOTO_PROFILE_PRINTER="C:\Program Files (x86)\Dry Creek Photo\Profile Target Printer\DCP Profile Target Printer.exe"
+set ADOBE_COLOR_PRINTER_UTILITY="%cd%\ACPU\Adobe Color Printer Utility.exe"
+
+
+
 echo Please print the TIFF files:
 dir /B /S %cd%\Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%*.tif
 
 echo ....
 
-for /f "tokens=*" %%G in ('dir /B /S %cd%\Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%*.tif') do (
-    echo %%G | clip
-    echo [101;93m Copied TIF file path to your clipboard[0m
-    echo [101;93m CTRL + V for TIF file path [0m
-    echo calling ACPU
-    "%cd%\ACPU\Adobe Color Printer Utility.exe"
+rem Check if Dry Creek Photo Profile Target Printer is installed
+if exist %DRY_CREEK_PHOTO_PROFILE_PRINTER% (
+    for /f "tokens=*" %%G in ('dir /B /S %cd%\Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%*.tif') do (
+        echo %%G | clip
+        echo [101;93m Copied TIF file path to your clipboard[0m
+        echo [101;93m CTRL + V for TIF file path [0m
+        echo calling Dry Creek Photo Profile Printer
+        %DRY_CREEK_PHOTO_PROFILE_PRINTER%
+    )
+) else (
+    for /f "tokens=*" %%G in ('dir /B /S %cd%\Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%*.tif') do (
+        echo %%G | clip
+        echo [101;93m Copied TIF file path to your clipboard[0m
+        echo [101;93m CTRL + V for TIF file path [0m
+        echo calling ACPU
+        %ADOBE_COLOR_PRINTER_UTILITY%
+    )
 )
+
 
 echo Step 3 Finished!
 echo Done printing profiles
