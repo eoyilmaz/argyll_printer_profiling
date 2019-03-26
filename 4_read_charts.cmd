8d9e052f2abace0f1db74978e61807c8d6c9a53d
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-27 02:58:02 +0300

    - 4_read_charts.cmd now asks the user the read mode, either Patch by Patch or Strip (default). - 4_read_charts.cmd now is now able to resume a half read chart. This combined with the read mode allows the user to fix errors on single patch with an unprecedented control on precision.

--- 4_read_charts.cmd
+++ 4_read_charts.cmd
@@ -1,5 +1,32 @@
 @echo off
-chartread -v -H -T0.4 Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+set READ_MODE_DEFAULT_VALUE=1
+set READ_MODE=
+set CONTINUE_READING_DEFAULT_VALUE=Y
+set CONTINUE_READING=
+
+set /p READ_MODE=Read Mode? Strip (1) or Patch by Patch (2)? (Default: 1-Strip)
+if "%READ_MODE%"=="" set READ_MODE=%READ_MODE_DEFAULT_VALUE%
+
+if exist Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%.ti3 (
+    set /p CONTINUE_READING=Previous Chart exists! Continue Reading? (%CONTINUE_READING_DEFAULT_VALUE%)
+) else (
+    set CONTINUE_READING=N
+)
+
+
+if "%READ_MODE%"=="1" (
+    if "%CONTINUE_READING%"=="Y" (
+        chartread -v -H -T0.4 -r Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+    ) else (
+        chartread -v -H -T0.4 Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+    )
+) else (
+    if "%CONTINUE_READING%"=="Y" (
+        chartread -v -H -T0.4 -p -P -r Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+    ) else (
+        chartread -v -H -T0.4 -p -P Outputs\%PRINTERBRAND%_%PRINTERMODEL%\%PROFILEDATE%\%PROFILENAME%
+    )
+)
 
 echo Step 4 Finished!
 echo Run Next Step (5_generate_profile)
