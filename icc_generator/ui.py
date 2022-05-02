f616b5e1e4bc4b12e1d428d2a18193205a91628a
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-05-02 18:16:25 +0100

    * [UI] Merged remote changes to the ``ui`` module.

--- icc_generator/ui.py
+++ icc_generator/ui.py
@@ -1,27 +1,71 @@
 # -*- coding: utf-8 -*-
+import sys
 
-from icc_generator import __version__
+import icc_generator
+from PySide2 import QtCore, QtGui, QtWidgets
 
 
-class UI(object):
-    """The UI
-    """
-    pass
+def ui_caller(app_in, executor, ui_class, **kwargs):
+    global app
+    global ui_instance
+    self_quit = False
+    try:
+        app = QtWidgets.QApplication.instance()
+    except TypeError:
+        app = None
+    if app is None:
+        if not app_in:
+            try:
+                app = QtWidgets.QApplication(sys.argv)
+            except (TypeError, AttributeError):  # sys.argv gives argv.error or
+                                                 # Qt gives TypeError
+                app = QtWidgets.QApplication([])
+        else:
+            app = app_in
+        self_quit = True
+
+    ui_instance = ui_class(**kwargs)
+    ui_instance.show()
+    if executor is None:
+        app.exec_()
+        if self_quit:
+            app.connect(
+                app,
+                QtCore.SIGNAL("lastWindowClosed()"),
+                app,
+                QtCore.SLOT("quit()")
+            )
+    else:
+        executor.exec_(app, ui_instance)
+    return ui_instance
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    """
+    """The main window
     """
 
     __company_name__ = 'Erkan Ozgur Yilmaz'
     __app_name__ = 'ICC Generator'
-    __version__ = __version__
+    __version__ = icc_generator.__version__
 
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent=parent)
+    def __init__(self, parent=None, *args, **kwargs):
+        super(MainWindow, self).__init__(parent=parent, *args, **kwargs)
+
+        self.central_widget = None
+        self.open_action = None
+        self.save_action = None
+        self.save_as_action = None
+        self.create_icc_profile_tab_widget = None
+
+        self.settings = QtCore.QSettings(
+            self.__company_name__,
+            self.__app_name__
+        )
         self.setup_ui()
 
     def setup_ui(self):
+        """creates the UI widgets
+        """
         self.setWindowTitle("%s v%s" % (self.__app_name__, self.__version__))
 
         self.create_main_menu()
@@ -60,18 +104,68 @@ class MainWindow(QtWidgets.QMainWindow):
     def create_main_menu(self):
         """creates the main application menu
         """
-        file_menu = self.menuBar().addMenu(self.tr(b"&File"))
+        file_menu = self.menuBar().addMenu(self.tr("&File"))
 
-        file_menu.addSeparator()
+        self.open_action = file_menu.addAction("&Open...")
+        self.save_action = file_menu.addAction("&Save")
+        self.save_as_action = file_menu.addAction("&Save As")
 
     def create_toolbars(self):
         """creates the toolbars
         """
-        file_toolbar = self.addToolBar(self.tr(b"File"))
+        file_toolbar = self.addToolBar(self.tr("File"))
         file_toolbar.setObjectName('file_toolbar')
-        create_project_action = file_toolbar.addAction('Create Project')
+        open_action = file_toolbar.addAction('Create Project')
+
+        # Create signals
+        # QtCore.QObject.connect(
+        #     open_action,
+        #     QtCore.SIGNAL('triggered()'),
+        #     self.create_project_action_clicked
+        # )
 
     def create_dock_widgets(self):
         """creates the dock widgets
         """
-        pass
+        # ----------------------------------------
+        # create the Project Dock Widget
+
+        # ----------------------------------------
+        # create the Central Widget
+        self.central_widget = ICCProfileTabWidget(parent=self)
+        self.setCentralWidget(self.central_widget)
+
+    def show_and_raise(self):
+        """
+        """
+        self.show()
+        self.raise_()
+
+    def closeEvent(self, event):
+        """The overridden close event
+        """
+        self.write_settings()
+        event.accept()
+
+
+class ICCProfileTabWidget(QtWidgets.QTabWidget):
+    """contains the widgets to create a new ICC profile
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(ICCProfileTabWidget, self).__init__(*args, **kwargs)
+        self.create_icc_profile_tab = None
+        self.color_correct_image_tab = None
+        self.setup_ui()
+
+    def setup_ui(self):
+        """sets the ui up
+        """
+        self.create_icc_profile_tab = QtWidgets.QWidget(self)
+        self.color_correct_image_tab = QtWidgets.QWidget(self)
+        self.addTab(self.create_icc_profile_tab, "Create ICC Profile")
+        self.addTab(self.color_correct_image_tab, "Color Correct Image")
+
+
+if __name__ == "__main__":
+    ui_caller(None, None, MainWindow)
